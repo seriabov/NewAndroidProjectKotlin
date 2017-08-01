a06d7cc7481427550a9c3cd83a74f4e2cfbde319
Sergei Riabov <stilroof@gmail.com>
2017-08-01 10:28:19 +0400

    enable LeakCanary for debug build type

--- app/src/debug/java/com/seriabov/newandroidprojectkotlin/util/BuildVariantUtil.kt
+++ app/src/debug/java/com/seriabov/newandroidprojectkotlin/util/BuildVariantUtil.kt
@@ -2,6 +2,7 @@ package com.seriabov.newandroidprojectkotlin.util
 
 import com.facebook.stetho.Stetho
 import com.seriabov.newandroidprojectkotlin.AppDelegate
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 /**
@@ -9,6 +10,12 @@ import timber.log.Timber
  */
 
 fun AppDelegate.onApplicationCreate() {
+
+  if (LeakCanary.isInAnalyzerProcess(this)) {
+    return
+  }
+  LeakCanary.install(this)
+
   Stetho.initializeWithDefaults(this)
 
   Timber.plant(Timber.DebugTree())
