047a487a9e9a52987d92baa9e4947ad8dc5b7f61
Sergei Riabov <stilroof@gmail.com>
2018-01-04 09:22:58 +0300

    update dependencies

--- app/src/main/java/com/seriabov/newandroidprojectkotlin/ui/common/BaseActivity.kt
+++ app/src/main/java/com/seriabov/newandroidprojectkotlin/ui/common/BaseActivity.kt
@@ -1,15 +1,11 @@
 package com.seriabov.newandroidprojectkotlin.ui.common
 
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
 import android.support.v7.app.AppCompatActivity
 
 /**
  * @author Sergei Riabov
  */
 
-open class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
-  private val lifecycleRegistry = LifecycleRegistry(this)
+open class BaseActivity : AppCompatActivity() {
 
-  override fun getLifecycle(): LifecycleRegistry = lifecycleRegistry
 }
